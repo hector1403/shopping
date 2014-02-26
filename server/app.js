3f8305caa36b7c0755d7bb7ad011e8c37e2cd2b5
Tarun Tyagi <metarun@gmail.com>
2014-02-25 23:19:17 -0600

    Adding express logic and mongodb

--- server/app.js
+++ server/app.js
@@ -1,4 +1,32 @@
 var express = require('express');
+var mongolian = require('mongolian');
+
 var app = express();
+app.use(express.bodyParser());
+var db = new mongolian('mongodb://taruntyagi14:marc1982@ds033629.mongolab.com:33629/meandb');
+
+app.put('/', function(req, res) {
+    var deal = req.body.deal_description;
+    var tags = req.body.all_tags;
+
+    console.log('Received deal: '+ deal);
+    console.log('Received all_tags: '+ tags);
+
+    db.collection("deals").insert({
+        deal: deal,
+        deal_tags: tags.split(",")
+    })
+
+    res.contentType('json');
+    res.send(JSON.stringify({ status: "success" }));
+});
+
+
+var port = process.env.PORT || 3001;
+
+app.listen(port, function() {
+   console.log('Listening on port:' + port);
+});
 
 module.exports = app;
+
